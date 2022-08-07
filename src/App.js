4e94b27dfb945fa88aeb715519841b490708049f
Jishin <jishink063@gmail.com>
2022-08-08 01:05:19 +0530

    v2

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import linkedIn from './asset/img/linkedIn.png'
 import gtiImage from './asset/img/gti.png'
 
 function App() {
+  let language =['HTML', 'CSS', 'JavaScript', 'React JS', 'Python', 'C++', 'Node JS', 'Power Fx', 'PowerShell'];
+  let tootls = ['VS Code', 'Git', 'DevOps', 'Share Gate']
   return (
     <div className="App">
 
@@ -18,26 +20,55 @@ function App() {
         </div>
         <div className='row d-flex' id='navRow'>
         <ul className="nav">
-          <li className="nav-item" id='spfx'>
+          <li className="nav-item mb-3" id='spfx'>
             <a className="nav-link active" aria-current="page" href="#">SPFx</a>
           </li>
-          <li className="nav-item" id='powerApps'>
+          <li className="nav-item mb-3" id='powerApps'>
             <a className="nav-link" href="#">PowerApps </a>
           </li>
-          <li className="nav-item" id='powerAuto'>
+          <li className="nav-item mb-3" id='powerAuto'>
             <a className="nav-link" href="#">Power Automate</a>
           </li>
-          <li className="nav-item" id='sharePoint'>
+          <li className="nav-item mb-3" id='sharePoint'>
             <a className="nav-link " href="#" tabindex="-1" aria-disabled="true">SharePoint</a>
           </li>
-          <li className="nav-item" id='web'>
+          <li className="nav-item mb-3" id='web'>
             <a className="nav-link " href="#" tabindex="-1" aria-disabled="true">Web</a>
           </li>
         </ul>
         </div>
+        <div className='row pt-4 pb-3'>
+            <h3 className='fs-4'>#Language of internet</h3>
+        </div>
         <div className='row skills'>
-
-
+            <div className='d-flex flex-wrap'>
+              {
+                  language.map(lan =>{
+                    return(
+                      <span className='roundShape px-2'>
+                        <p style={{color:'#FFB900'}}>{lan}</p>
+                      </span>
+                    )
+                  })
+              }
+            </div>
+        </div>
+        <div className='row pt-4 pb-3'>
+            <h3 className='fs-4'>#Tools</h3>
+        </div>
+        <div className='row skills'>
+            <div className='d-flex flex-wrap'>
+            {
+                  tootls.map(too =>{
+                    return(
+                      <span className='roundShape px-2'>
+                        <p style={{color:'#7FBA00'}}>{too}</p>
+                      </span>
+                    )
+                  })
+              }
+       
+            </div>
         </div>
       </div>
     </div>
