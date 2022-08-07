4147b60fbdfd499ddee9deb2d6188b3a193087e1
Jishin <jishink063@gmail.com>
2022-08-07 19:48:44 +0530

    new version | Jishin

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Blogs from './pages/blog'
 import 'bootstrap/dist/css/bootstrap.css';
+import linkedIn from './asset/img/linkedIn.png'
+import gtiImage from './asset/img/gti.png'
 
 function App() {
   return (
@@ -11,8 +13,8 @@ function App() {
       <div className='container mainContent'>
 
         <div className='row pt-5'>
-            <h1>Jishin Kathiran</h1>
-            <p>SharePoint and PowerPlatform developer</p>
+            <h1 className=' title'>Jishin Kathiran  <a href='https://www.linkedin.com/in/jishinkathiran/' target='_blank'><img src={linkedIn} width='45'/></a> <a href='https://www.linkedin.com/in/jishinkathiran/' target='_blank'><img src={gtiImage} width='50' /></a> </h1>
+            <p className='des'>SharePoint and PowerPlatform developer </p>
         </div>
         <div className='row d-flex' id='navRow'>
         <ul class="nav">
